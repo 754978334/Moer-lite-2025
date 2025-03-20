550b56f00acfe932a174680487d3c28ae7b99eda
sal <754978334@qq.com>
2025-03-20 13:27:09 +0800

    lab1: fill intersection method

--- src/FunctionLayer/Shape/Triangle.cpp
+++ src/FunctionLayer/Shape/Triangle.cpp
@@ -16,7 +16,37 @@ Triangle::Triangle(int _primID, int _vtx0Idx, int _vtx1Idx, int _vtx2Idx,
 
 bool Triangle::rayIntersectShape(Ray &ray, int *primID, float *u,
                                  float *v) const {
-  //* todo 实现三角形与光线求交
+  Point3f vtx0 = mesh->transform.toWorld(mesh->meshData->vertexBuffer[vtx0Idx]),
+  vtx1 = mesh->transform.toWorld(mesh->meshData->vertexBuffer[vtx1Idx]),
+  vtx2 = mesh->transform.toWorld(mesh->meshData->vertexBuffer[vtx2Idx]);
+
+  float eps = std::numeric_limits<float>::epsilon();
+
+  auto S = ray.origin - vtx0;
+  auto E1 = vtx1 - vtx0;
+  auto E2 = vtx2 - vtx0;
+  auto S1 = cross(ray.direction, E2);
+  auto S2 = cross(S, E1);
+
+  float S1E1 = dot(S1, E1);
+  if(std::abs(S1E1) < eps)
+      return false;
+
+  float t = dot(S2, E2) / S1E1;
+  float b1 = dot(S1, S) / S1E1;
+  float b2 = dot(S2, ray.direction) / S1E1;
+
+  if(t > eps && b1 >= 0.f && b2 >= 0.f && (1 - b1 - b2) >= 0.f)
+  {
+    if(t < ray.tFar){
+      ray.tFar = t;
+      *primID = this->primID;
+      *u = b1;
+      *v = b2;
+      return true;
+    }
+  }
+
   return false;
 }
 
@@ -66,13 +96,54 @@ bool TriangleMesh::rayIntersectShape(Ray &ray, int *primID, float *u,
 
 void TriangleMesh::fillIntersection(float distance, int primID, float u,
                                     float v, Intersection *intersection) const {
-  //* todo 填充光线与三角网格求交得到的交点信息
+  
   intersection->distance = distance;
   intersection->shape = this;
+  std::array<DataIndex, 3> vtxIdxs = meshData->faceBuffer[primID];
   //* 1. 在三角形内部用插值计算交点坐标
+  Point3f p0 = transform.toWorld(meshData->vertexBuffer[vtxIdxs[0].vertexIndex]);
+  Point3f p1 = transform.toWorld(meshData->vertexBuffer[vtxIdxs[1].vertexIndex]);
+  Point3f p2 = transform.toWorld(meshData->vertexBuffer[vtxIdxs[2].vertexIndex]);
+  intersection->position = {
+    p0[0] * (1 - u - v) + p1[0] * u + p2[0] * v,
+    p0[1] * (1 - u - v) + p1[1] * u + p2[1] * v,
+    p0[2] * (1 - u - v) + p1[2] * u + p2[2] * v
+  };
   //* 2. 在三角形内部用插值计算法线
+  Vector3f p0n = transform.toWorld(meshData->normalBuffer[vtxIdxs[0].normalIndex]);
+  Vector3f p1n = transform.toWorld(meshData->normalBuffer[vtxIdxs[1].normalIndex]);
+  Vector3f p2n = transform.toWorld(meshData->normalBuffer[vtxIdxs[2].normalIndex]);
+  intersection->normal = {
+    p0n[0] * (1 - u - v) + p1n[0] * u + p2n[0] * v,
+    p0n[1] * (1 - u - v) + p1n[1] * u + p2n[1] * v,
+    p0n[2] * (1 - u - v) + p1n[2] * u + p2n[2] * v,
+  };
   //* 3. 在三角形内部用插值计算纹理坐标
+  Vector2f p0t = meshData->texcodBuffer[vtxIdxs[0].texcodIndex];
+  Vector2f p1t = meshData->texcodBuffer[vtxIdxs[1].texcodIndex];
+  Vector2f p2t = meshData->texcodBuffer[vtxIdxs[2].texcodIndex];
+  intersection->texCoord = {
+    p0t[0] * (1 - u - v) + p1t[0] * u + p2t[0] * v,
+    p0t[1] * (1 - u - v) + p1t[1] * u + p2t[1] * v,
+    p0t[2] * (1 - u - v) + p1t[2] * u + p2t[2] * v,
+  };
   //* 4. 在三角形内部用插值计算交点的切线和副切线
+  Vector3f e1 = p1 - p0;
+  Vector3f e2 = p2 - p0;
+  Vector2f duv1 = p1t - p0t;
+  Vector2f duv2 = p2t - p0t;
+
+  float f = 1.0f / (duv1.x() * duv2.y() - duv1.y() * duv2.x());
+  intersection->tangent = {
+    f * (duv2.y() * e1[0] - duv1.y() * e2[0]),
+    f * (duv2.y() * e1[1] - duv1.y() * e2[1]),
+    f * (duv2.y() * e1[2] - duv1.y() * e2[2])
+  };
+  intersection->bitangent = {
+    f * (-duv2.x() * e1[0] + duv1.x() * e2[0]),
+    f * (-duv2.x() * e1[1] + duv1.x() * e2[1]),
+    f * (-duv2.x() * e1[2] + duv1.x() * e2[2]),
+  };
 }
 
 void TriangleMesh::initInternalAcceleration() {
